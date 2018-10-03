85477364b60af728def2f250f5c6919432a5d2b0
Tristan Brown <tgbrown@stanford.edu>
2018-10-03 21:28:52 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,30 +1,30 @@
-class Doctor 
+class Doctor
   @@all = []
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
+  attr_accessor :name
 
-  def self.all 
-    @@all  
-  end 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
   def new_appointment(patient, date)
     Appointment.new(self, patient, date)
-  end 
+  end
 
-  def appointments 
+  def appointments
     Appointments.all.collect do |appointments|
-      appointments.doctor = self 
-    end 
-  end 
+      appointments.doctor = self
+    end
+  end
 
-  def patients 
+  def patients
     Appointments.all.collect do |appointment|
       appointment.patient
-    end 
+    end
   end 
 
-end 
+end
