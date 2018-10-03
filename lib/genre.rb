398d45f1655e0d805e03afb19df046394edcf6cc
Tristan Brown <tgbrown@stanford.edu>
2018-10-03 21:21:41 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,24 @@
+class Genre
 
+  @@all = []
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+
+  def songs
+    Song.all collect do |song|
+      song.genre = self 
+    end 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def artists
+  end
+end
