e3a40ce8576d1dccc25c6239d8caac8040031d1a
Tristan Brown <tgbrown@stanford.edu>
2018-10-03 21:41:03 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    appointment = Appointment.new(self, date, patient)
+    appointment = Appointment.new(self, patient, date)
   end
 
   def appointments
